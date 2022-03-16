a5160bf4455d7ddd9119c20eba122f8060652d9c
MexxaDev <44383860+MexxaDev@users.noreply.github.com>
2022-03-15 22:18:55 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+alert("Por favor preciona F11 para poner la pagina en pantalla completa");
+
+const Hora = new Date();
+let h = Hora.getHours();
+let m = Hora.getMinutes();
+let s = Hora.getSeconds();
+
+
+
+var HoraActual = document.getElementById("hora");
+var MinutoActual = document.getElementById("minuto");
+var SegundoActual = document.getElementById("segundo");
+
+
+
+var time = h + ":" + m + ":" + s;
+
+HoraActual.innerHTML = time
